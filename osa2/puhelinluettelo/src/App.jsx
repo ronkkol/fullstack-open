216fe277d459d0c910a3132960a9e5713f18471a
Leevi Rönkkö <leevi.ronkko@aalto.fi>
2024-09-29 11:54:03 +0300

    Osa2: 2.15 asti

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -1,29 +1,38 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+
+import client from './personService'
 
 const NameFilter = ({ filter, onFilterChange }) => (
   <div>
     filter shown with <input value={filter} onChange={onFilterChange} />
   </div>
 )
-const PersonForm = ({ onSubmit, onNameChange, onNumberChange }) => (
+const PersonForm = ({
+  onSubmit,
+  onNameChange,
+  onNumberChange,
+  name,
+  number
+}) => (
   <form onSubmit={onSubmit}>
     <div>
-      name: <input onChange={onNameChange} />
+      name: <input onChange={onNameChange} value={name} />
     </div>
     <div>
-      number: <input onChange={onNumberChange} />
+      number: <input onChange={onNumberChange} value={number} />
     </div>
     <div>
       <button type="submit">add</button>
     </div>
   </form>
 )
-const PersonList = ({ persons, filter }) => (
+const PersonList = ({ onDelete, persons, filter }) => (
   <ul>
     {persons.map((person) =>
       person.name.toLowerCase().includes(filter.toLowerCase()) ? (
-        <li key={person.name}>
-          {person.name} {person.number}
+        <li key={person.id}>
+          {person.name} {person.number}{' '}
+          <button onClick={() => onDelete(person)}>delete</button>
         </li>
       ) : null
     )}
@@ -31,25 +40,63 @@ const PersonList = ({ persons, filter }) => (
 )
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
 
+  useEffect(() => {
+    client.getAll().then((data) => {
+      console.log(data)
+      setPersons(data)
+    })
+  }, [])
+
   const onSubmit = (event) => {
     event.preventDefault()
-    if (persons.find((person) => person.name === newName)) {
-      alert(`${newName} is already added to phonebook`)
+    const existing = persons.find((person) => person.name === newName)
+    if (existing) {
+      if(window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)) {
+        const newValue = { ...existing, number: newNumber }
+        client.update(existing.id, newValue).then((data) => {
+          setPersons(persons.map((p) => (p.id !== existing.id ? p : data)))
+        }).catch((error) => {
+          alert(error)
+        })
+        .finally(() => {
+          setNewName('')
+          setNewNumber('')
+        })
+      }
       return
     }
 
-    setPersons(persons.concat({ name: newName, number: newNumber }))
+    client
+      .create({ name: newName, number: newNumber })
+      .then((data) => {
+        setPersons(persons.concat(data))
+      })
+      .catch((error) => {
+        alert(error)
+      })
+      .finally(() => {
+        setNewName('')
+        setNewNumber('')
+      })
   }
+  const onDelete = (person) => {
+    if (window.confirm(`Delete ${person.name}?`)) {
+      client
+        .deletePerson(person.id)
+        .then(() => {
+          setPersons(persons.filter((p) => p.id !== person.id))
+        })
+        .catch((error) => {
+          alert(error)
+        })
+    }
+  }
+
   const onNameChange = (event) => {
     setNewName(event.target.value)
   }
@@ -69,9 +116,11 @@ const App = () => {
         onSubmit={onSubmit}
         onNameChange={onNameChange}
         onNumberChange={onNumberChange}
+        name={newName}
+        number={newNumber}
       />
       <h2>Numbers</h2>
-      <PersonList persons={persons} filter={filter} />
+      <PersonList persons={persons} filter={filter} onDelete={onDelete} />
     </div>
   )
 }

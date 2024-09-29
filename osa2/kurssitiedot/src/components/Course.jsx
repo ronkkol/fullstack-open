a0b06f03b92f5cba8b9be46f3217dc98da254980
Leevi Rönkkö <leevi.ronkko@aalto.fi>
2024-09-29 09:51:38 +0300

    Osa2: kurssitiedot

---
+++ osa2/kurssitiedot/src/components/Course.jsx
@@ -0,0 +1,37 @@
+const Header = ({ course }) => {
+  return <h1>{course.name}</h1>
+}
+
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  )
+}
+
+const Content = ({ parts }) => {
+  return (
+    <>
+      {parts.map((part) => (
+        <Part key={part.id} part={part} />
+      ))}
+    </>
+  )
+}
+
+const Total = ({ parts }) => {
+  return <b>total of {parts.reduce((a, b) => a + b.exercises, 0)} excersises</b>
+}
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+export default Course
